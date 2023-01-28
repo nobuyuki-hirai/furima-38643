64e1f382d7e5212d0661bf69f0cd8c9ab2b33cd0
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-01-28 20:04:04 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true
   validates :last_name,             presence: true
   validates :first_name,            presence: true
-  validates :last_name_kana         presence: true
-  validates :first_name_kana        presence: true
-  validates :birthday               presence: true
+  validates :last_name_kana,        presence: true
+  validates :first_name_kana,       presence: true
+  validates :birthday,              presence: true
 
   has_many :items
   has_many :buys
