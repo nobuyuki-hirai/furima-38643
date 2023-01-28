210aa94954451d442ce679c01ac869c340c87d18
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-01-28 22:42:44 +0900

    バリデーション(email,password)削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,              presence: true
-  validates :email,                 presence: true
-  validates :password,              presence: true
   validates :password_confirmation, presence: true
   validates :last_name,             presence: true
   validates :first_name,            presence: true
