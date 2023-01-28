7a60f382ebda10744ff22e2c1097a70a05f8a31b
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-01-29 00:44:46 +0900

    パスワード バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,              presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :password_confirmation, presence: true
   validates :last_name,             presence: true
   validates :first_name,            presence: true
