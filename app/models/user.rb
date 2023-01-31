2a64baecc63cb84c0368ebc607829ac750c0a52a
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-01 04:18:35 +0900

    バリデーションのコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,6 @@ class User < ApplicationRecord
   validates :first_name_kana,       presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday,              presence: true
 
-  has_many :items
-  has_many :buys
+  #has_many :items
+  #has_many :buys
 end
