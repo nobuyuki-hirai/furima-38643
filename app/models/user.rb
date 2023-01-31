793e33a51e661c4ca8b241cdd1f7da2b4fd397ad
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-01 04:19:24 +0900

    password_confirmation 削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   validates :nickname,              presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  validates :password_confirmation, presence: true
   validates :last_name,             presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :first_name,            presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :last_name_kana,        presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
