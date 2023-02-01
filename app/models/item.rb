14b5e12c533d364b525fd231d534047dee80bacb
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-01 23:03:54 +0900

    itemモデル　誤字修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
   validates :title,            presence: true
-  varidates :price,            presence: true
+  validates :price,            presence: true
   validates :concept,          presence: true
-  varidates :category_id       presence: true
-  validates :condition_id      presence: true
-  varidates :postage_payer_id  presence: true
-  validates :ship_area_id      presence: true
-  validates :ship_date_id      presence: true
+  validates :category_id,      presence: true
+  validates :condition_id,     presence: true
+  validates :postage_payer_id, presence: true
+  validates :ship_area_id,     presence: true
+  validates :ship_date_id,     presence: true
 
   belongs_to :user
 end
