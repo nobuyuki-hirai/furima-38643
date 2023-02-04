72d23a46e11e7415776f6050b02968933fdd24d4
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-04 19:18:04 +0900

    Active_Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,7 @@ class Item < ApplicationRecord
   validates :ship_date_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank" }
 
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
