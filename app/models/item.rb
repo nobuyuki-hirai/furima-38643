121a93434de56d0e9af96428b4ac5364b9bfe2c7
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-04 17:53:40 +0900

    condition バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   validates :price,            presence: true
   validates :concept,          presence: true
   validates :category_id,      presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :condition_id,     presence: true
+  validates :condition_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :postage_payer_id, presence: true
   validates :ship_area_id,     presence: true
   validates :ship_date_id,     presence: true
@@ -12,4 +12,6 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
+  
 end
