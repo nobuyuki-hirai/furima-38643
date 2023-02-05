f5b199d0c7c12409b68f13375f65faf008737098
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-06 02:33:03 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,14 @@
 class Item < ApplicationRecord
   validates :title,            presence: true
-  validates :price,            presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price,            presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" }
+  validates :price,                            numericality: { only_integer: true, message: "Half-width number."}
   validates :concept,          presence: true
   validates :image,            presence: true
-  validates :category_id,      presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :condition_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :postage_payer_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :ship_area_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :ship_date_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :category_id,      numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :condition_id,     numericality: { other_than: 1 , message: "can't be blank" }
+  validates :postage_payer_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :ship_area_id,     numericality: { other_than: 1 , message: "can't be blank" }
+  validates :ship_date_id,     numericality: { other_than: 1 , message: "can't be blank" }
 
   belongs_to :user
   has_one_attached :image
@@ -15,7 +16,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
-  belongs_to :postagepayer
-  belongs_to :shiparea
-  belongs_to :shipdate
+  belongs_to :postagepayer, optional: true
+  belongs_to :shiparea,     optional: true
+  belongs_to :shipdate,     optional: true
 end
