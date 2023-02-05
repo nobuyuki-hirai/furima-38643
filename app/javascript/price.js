c8d1503add6b1ddaa6c4e62577a49450e92f58e3
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-05 22:37:41 +0900

    Javascript 販売手数料の表示

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,9 +1,12 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', () => {
 
-  const priceInput = document.getElementById("item-price")
+  const priceInput = document.getElementById("item-price");
 
-  priceInput.addEventListener('keyup', () => {
-    const inputValue = priceInput.value;
-    console.log(inputValue);
+  priceInput.addEventListener('input', () => {
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.round(priceInput.value * 0.1);
+
+    const addPofitDom = document.getElementById("profit");
+    addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
   })
-})
+});
