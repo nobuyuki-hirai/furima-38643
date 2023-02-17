70a362efdd5f1c61736f10d5daa774df29752b11
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-17 23:24:18 +0900

    rails db:migrate, buys, buyers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_12_122125) do
+ActiveRecord::Schema.define(version: 2023_02_17_141118) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -34,18 +34,16 @@ ActiveRecord::Schema.define(version: 2023_02_12_122125) do
   end
 
   create_table "buyers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "post_code", null: false
-    t.integer "ship_area_id", null: false
+    t.string "post_doce", null: false
+    t.integer "prefecture", null: false
     t.string "city", null: false
     t.string "address", null: false
     t.string "build_name", null: false
     t.string "phone_number", null: false
-    t.bigint "user_id", null: false
-    t.bigint "item_id", null: false
+    t.bigint "buy_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_buyers_on_item_id"
-    t.index ["user_id"], name: "index_buyers_on_user_id"
+    t.index ["buy_id"], name: "index_buyers_on_buy_id"
   end
 
   create_table "buys", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -91,8 +89,7 @@ ActiveRecord::Schema.define(version: 2023_02_12_122125) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "buyers", "items"
-  add_foreign_key "buyers", "users"
+  add_foreign_key "buyers", "buys"
   add_foreign_key "buys", "items"
   add_foreign_key "buys", "users"
   add_foreign_key "items", "users"
