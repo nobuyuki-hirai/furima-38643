41663c88def61e5fb6b0adca03254c6c68a95598
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-19 15:48:45 +0900

    orders db:migrate

--- db/migrate/20230219045347_create_orders.rb
+++ db/migrate/20230219045347_create_orders.rb
@@ -1,6 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
+      t.integer      :customer_id,        null: false
       t.references   :user,               null: false, foreign_key: true
       t.references   :item,               null: false, foreign_key: true
       t.timestamps
