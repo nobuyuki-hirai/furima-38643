57a32c2bbb59925f8f2e15713cb9df3865ea71ed
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-19 15:37:16 +0900

    item model 修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
 
   belongs_to       :user
   has_one_attached :image
-  has_one          :buy
+  has_one          :order
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
