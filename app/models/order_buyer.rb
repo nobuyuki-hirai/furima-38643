5f16bfa6d0e916b41046d9b46071dbff7d2091bd
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-19 18:44:56 +0900

    order_buyer.rb model　作成

---
+++ app/models/order_buyer.rb
@@ -0,0 +1,16 @@
+class OrderBuyer
+  include ActiveModel::Model
+  attr_accessor :post_code, :prefecture, :city, :address, :build_name, :phone_number, :user_id, :order_id, :item_id
+
+  with_options presence: true do
+    validates  :post_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates  :phone_number, format: { with: /\A\d{10,11}\z/, message: "PhoneNumber must be 10or11 digit Half-width numbers" }
+    validates  :city, :address, :user_id, :item_id
+  end
+  validates  :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    order = Order.create(customer_id: customer_id, user_id: user_id, item_id: item_id)
+    Buyer.create(post_code: post_code, prefecture: prefecture, city: city, address: address, build_name: build_name, phone_number: phone_number, order_id: order.id)
+  end
+end
