7f7c1f4885e168ba5befa3efc834cbfe9f3e0f32
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-19 18:44:40 +0900

    orders_controller.rb 修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,15 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    @order = Order.new
+    @order_buyer = OrderBuyer.new
   end
 
   def create
-    binding.pry
-    @order = Order.new(order_params)
-    if @order.valid?
-      @order.save
-      return redirect_to root_path
+    @item = Item.find(params[:item_id])
+    @order_buyer = OrderBuyer.new(order_params)
+    if @order_buyer.valid?
+      @order_buyer.save
+      redirect_to root_path
     else
       render 'index'
     end
@@ -18,6 +18,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order)
+    params.permit(:customer_id, :post_code, :prefwcture, :city, :address, :build_name, :phone_number).merge(user_id: current_user.id)
   end
 end
