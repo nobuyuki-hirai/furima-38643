c7e6fe6a8e0c7be83a7b7e7be5a702d83ff039af
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-21 00:03:44 +0900

    orders_controller.rb 売却済み root_path遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,24 +1,38 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+
   def index
     @item = Item.find(params[:item_id])
     @order_buyer = OrderBuyer.new
+    if current_user == @item.user || @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def create
     @item = Item.find(params[:item_id])
     @order_buyer = OrderBuyer.new(order_params)
-    binding.pry
     if @order_buyer.valid?
+      pay_item
       @order_buyer.save
       redirect_to root_path
     else
-      render 'index'
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.permit(:post_code, :prefecture, :city, :address, :build_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.permit(:post_code, :ship_area_id, :city, :address, :build_name, :phone_number, :token, :item_id).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
   end
 end
