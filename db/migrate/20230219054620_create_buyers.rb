f2b72bc9f15c162e4ba3f3878c5a0431ee86f6e2
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-24 21:38:59 +0900

    buyers カラム名修正

--- db/migrate/20230219054620_create_buyers.rb
+++ db/migrate/20230219054620_create_buyers.rb
@@ -2,7 +2,7 @@ class CreateBuyers < ActiveRecord::Migration[6.0]
   def change
     create_table :buyers do |t|
       t.string         :post_code,      null: false
-      t.integer        :ship_area_id,   null: false
+      t.integer        :prefecture,     null: false
       t.string         :city,           null: false
       t.string         :address,        null: false
       t.string         :build_name
