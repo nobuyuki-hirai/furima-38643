edebfa2c441df3c316f60ba04b39b913ad24bb12
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-02-26 14:48:41 +0900

    models/order_buyer.rb post_code修正

--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -3,7 +3,7 @@ class OrderBuyer
   attr_accessor :post_code, :prefecture, :city, :address, :build_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates  :post_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates  :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates  :phone_number, format: { with: /\A\d{10,11}\z/, message: 'PhoneNumber must be 10or11 digit Half-width numbers' }
     validates  :city, :address, :user_id, :item_id, :token
   end
