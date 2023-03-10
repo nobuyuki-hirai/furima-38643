5ce34131986fb6035ca58314ff0a601c69334168
nobuyuki-hirai <117642632+nobuyuki-hirai@users.noreply.github.com>
2023-03-10 18:39:30 +0900

    deploy.rb 誤字修正 furima-38643

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.17.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'furima-38642'
+set :application, 'furima-38643'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:nobuyuki-hirai/furima-38643.git'
